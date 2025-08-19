ae11b0bb24b629152f46217ece378e4601832c2d
gerald-gv <geraldgutierrez918@gmail.com>
2025-08-19 01:15:40 -0500

    Se arrelgo el desborde en Inicio

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -18,7 +18,7 @@ const Layout = () => {
   return (
     <div className='min-h-screen flex flex-col'>
         <Navbar />
-        <main className="flex-grow">
+        <main className="flex-grow overflow-x-hidden">
             <Outlet/>
         </main>
         <Footer />
